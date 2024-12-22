30f65510270758c16afddd56e6560799ead40425
CharanGill <charangill2008@gmail.com>
2024-12-22 01:56:34 +0000

    Add functionality to question class with variables, constructors, getters, and validation

--- multipleChoiceQnA/src/qnaApp/Question.java
+++ multipleChoiceQnA/src/qnaApp/Question.java
@@ -1,5 +1,50 @@
 package qnaApp;
 
-public class Question {
+import java.util.Arrays;
 
+public class Question {
+	
+	private String question;
+	private String[] options;
+	private int answer;
+	
+	public Question(String question, String[] options, int answer) {
+		
+		if (options == null || options.length > 4) {
+			throw new IllegalArgumentException("Must be between 1 and 4 choices!");
+		}
+		
+		if (answer > 1) {
+			throw new IllegalArgumentException("Must only be 1 answer!");
+		}
+		
+		this.question = question;
+		this.options = options;
+		this.answer = answer;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+
+	public String[] getOptions() {
+		return options;
+	}
+
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	@Override
+	public String toString() {
+		String print = question + "\n";
+		for (String option: options) {
+			print += option + "\n"; 
+		}
+		print += "Answer: " + answer + "\n";
+		return print;
+	}
 }
+
