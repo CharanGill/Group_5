b61c5b52d635c9432bd8f002b7473eee4aeb76f6
CharanGill <charangill2008@gmail.com>
2024-12-23 02:03:11 +0000

    Create method for reading from files with exception handling

--- multipleChoiceQnA/src/qnaApp/Quiz.java
+++ multipleChoiceQnA/src/qnaApp/Quiz.java
@@ -1,5 +1,33 @@
 package qnaApp;
 
-public class Quiz {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Quiz {
+	
+	private ArrayList<Question> questions = new ArrayList<>();
+	
+	public void loadQuestions() {
+	
+		try {
+			File myObj = new File("filename.txt");
+			Scanner myReader = new Scanner(myObj);
+		
+			String line;
+			String question;
+			String[] options = null;
+		
+			while(myReader.hasNextLine()) {
+				line = myReader.nextLine();
+			}
+			
+			myReader.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("No file found!");
+		}
+	
+	}
 }
